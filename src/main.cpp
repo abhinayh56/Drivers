bd5250cd275b60f3ee0171edf9e5eadbddc0c11a
abhinayh56 <kumarabhinay53@gmail.com>
2025-07-16 15:56:53 +0530

    asfdasf

--- src/main.cpp
+++ src/main.cpp
@@ -1,58 +1,60 @@
 #include "Arduino.h"
 #include "../Drivers/I2C_bus/src/I2C_bus.h"
-#include "../Drivers/MPU6050/src/MPU6050.h"
+#include "../Drivers/MS5611/src/MS5611.h"
 #include "Wire.h"
+#include <stdint.h>
 
 I2C_bus<TwoWire> i2c_bus(&Wire);
-MPU6050<I2C_bus<TwoWire>> imu(&i2c_bus, (uint8_t)MPU6050_INFO::ADDR::I2C_DEFAULT);
+MS5611<I2C_bus<TwoWire>> bar(&i2c_bus, (uint8_t)MS5611_INFO::ADDR::I2C_DEFAULT);
 
 float SampleFrequency = 500.0, dt = 1.0/SampleFrequency;
 unsigned long loop_timer = 1000000.0*dt, t;
 
-int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
+double press = 0;
+double temp = 0;
 
 unsigned long t_n = 0;
 
-void print_raw_sensor_data();
 void wait();
 
-void setup(){
-  Serial.begin(115200);
-
-  i2c_bus.init();
-  i2c_bus.config(400000);
-  imu.init();
-  imu.config();
-  
-  Serial.println("---------------------------");
-  Serial.println("Initializing test!");
-
-  t = micros();
-}
-
-void loop(){
-  t_n = micros();
-  Serial.print((float)millis()/1000.0); Serial.print('\t');
-  imu.get_data(&AcX, &AcY, &AcZ, &Tmp, &GyX, &GyY, &GyZ);
-  print_raw_sensor_data();
-
-  Serial.print(1000000.0/double(micros()-t_n)); Serial.print("\t");
-  wait();
-  Serial.println(1000000.0/double(micros()-t_n));
-}
-
-void print_raw_sensor_data()
+void setup()
 {
-  Serial.print(AcX); Serial.print('\t');
-  Serial.print(AcY); Serial.print('\t');
-  Serial.print(AcZ); Serial.print('\t');
-  Serial.print(Tmp); Serial.print('\t');
-  Serial.print(GyX); Serial.print('\t');
-  Serial.print(GyY); Serial.print('\t');
-  Serial.print(GyZ); Serial.print('\t');
+	Serial.begin(115200);
+
+	i2c_bus.init();
+	i2c_bus.config(400000);
+	bar.init();
+	delay(5);
+	bar.config(4096, 4096, (uint16_t)loop_timer, 3);
+
+	Serial.println("---------------------------");
+	Serial.println("Initializing test!");
+
+	t = micros();
 }
 
-void wait(){
-  while(micros()-t<loop_timer){}
-  t = micros();
+void loop()
+{
+	t_n = micros();
+	Serial.print((float)millis() / 1000.0);
+	Serial.print('\t');
+	bar.get_data(&press, &temp);
+
+	Serial.print(press);
+	Serial.print('\t');
+	Serial.print(temp);
+	Serial.print('\t');
+
+	Serial.print(1000000.0 / double(micros() - t_n));
+	Serial.print("\t");
+	wait();
+	Serial.println(1000000.0 / double(micros() - t_n));
+}
+
+void wait()
+{
+	while (micros() - t < loop_timer)
+	{
+	}
+	t = micros();
 }
