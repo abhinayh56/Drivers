0bc5c2967b6752762cb141cf7fd2c82e9ff54a0f
abhinayh56 <kumarabhinay53@gmail.com>
2025-07-17 00:05:44 +0530

    adfasdf

--- test/main_test.h
+++ test/main_test.h
@@ -1,6 +1,7 @@
 // #define TEST_MPU6050
 // #define TEST_HMC5883
 // #define TEST_MS5611
+#define TEST_UART_0
 
 #ifdef TEST_MPU6050
 #include "../lib/Drivers/MPU6050/test/MPU6050.ino"
@@ -10,6 +11,34 @@
 
 #elif defined(TEST_MS5611)
 #include "../lib/Drivers/MS5611/test/MS5611.ino"
+
+#elif defined(TEST_UART_0)
+#include "../lib/HAL/Common/UART_bus/UART_bus.h"
+#include <Arduino.h>
+
+UART_bus<usb_serial_class> uart_0(&Serial);
+
+void setup()
+{
+    UART_param::Param uart_0_param{115200,
+                                   UART_param::Data_bits::BITS_8,
+                                   UART_param::Stop_bits::BITS_1,
+                                   UART_param::Parity::NONE,
+                                   UART_param::Flow_control::NO_FLOW_CONTROL,
+                                   UART_param::Mode::TRANSMIT_RECEIVE};
+
+    uart_0.config(uart_0_param);
+
+    delay(5000);
+}
+
+void loop()
+{
+    uart_0.print("This is single line 1");
+    uart_0.println("This is single line 2");
+    uart_0.print(56);
+    delay(1000);
+}
 
 #else
 #include <Arduino.h>
