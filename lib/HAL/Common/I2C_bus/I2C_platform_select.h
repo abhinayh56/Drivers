5cdf73ddc0ef2144a15f86abbe14ca3c81cc3a5b
abhinayh56 <kumarabhinay53@gmail.com>
2025-07-16 19:05:15 +0530

    sdgasdf

--- lib/HAL/Common/I2C_bus/I2C_platform_select.h
+++ lib/HAL/Common/I2C_bus/I2C_platform_select.h
@@ -1,14 +1,14 @@
 #ifndef HAL_I2C_H
 #define HAL_I2C_H
 
-#ifdef ARDUINO
+#ifdef ARDUINO_TEENSY41
+#include "../../HAL_teensy/I2C_bus/I2C_bus.tpp"
+
+#elif defined(ARDUINO_ESP32)
+#include ""
+
+#elif defined(ARDUINO)
 #include "../../HAL_arduino/I2C_bus/I2C_bus.tpp"
-
-#elif defined(ESP32)
-#include ""
-
-#elif defined(TEENSY_4_1)
-#include ""
 
 #else
 #include ""
