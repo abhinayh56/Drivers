2d47161594609db75b058a852e3fd4a1f68f2917
abhinayh56 <kumarabhinay53@gmail.com>
2025-07-16 16:00:58 +0530

    asdfsadf

--- src/main.cpp
+++ src/main.cpp
@@ -1,60 +1,55 @@
 #include "Arduino.h"
 #include "../Drivers/I2C_bus/src/I2C_bus.h"
-#include "../Drivers/MS5611/src/MS5611.h"
+#include "../Drivers/HMC5883/src/HMC5883.h"
 #include "Wire.h"
 #include <stdint.h>
 
 I2C_bus<TwoWire> i2c_bus(&Wire);
-MS5611<I2C_bus<TwoWire>> bar(&i2c_bus, (uint8_t)MS5611_INFO::ADDR::I2C_DEFAULT);
+HMC5883<I2C_bus<TwoWire>> mag(&i2c_bus, (uint8_t)HMC5883_INFO::ADDR::I2C_DEFAULT);
 
 float SampleFrequency = 500.0, dt = 1.0/SampleFrequency;
 unsigned long loop_timer = 1000000.0*dt, t;
 
-double press = 0;
-double temp = 0;
+int16_t MgX, MgY, MgZ;
 
 unsigned long t_n = 0;
 
+void print_raw_sensor_data();
 void wait();
 
-void setup()
-{
-	Serial.begin(115200);
+void setup(){
+  Serial.begin(115200);
 
-	i2c_bus.init();
-	i2c_bus.config(400000);
-	bar.init();
-	delay(5);
-	bar.config(4096, 4096, (uint16_t)loop_timer, 3);
+  i2c_bus.init();
+  i2c_bus.config(400000);
+  mag.init();
+  mag.config();
+  
+  Serial.println("---------------------------");
+  Serial.println("Initializing test!");
 
-	Serial.println("---------------------------");
-	Serial.println("Initializing test!");
-
-	t = micros();
+  t = micros();
 }
 
-void loop()
-{
-	t_n = micros();
-	Serial.print((float)millis() / 1000.0);
-	Serial.print('\t');
-	bar.get_data(&press, &temp);
+void loop(){
+  t_n = micros();
+  Serial.print((float)millis()/1000.0); Serial.print('\t');
+  mag.get_data(&MgX, &MgY, &MgZ);
+  print_raw_sensor_data();
 
-	Serial.print(press);
-	Serial.print('\t');
-	Serial.print(temp);
-	Serial.print('\t');
-
-	Serial.print(1000000.0 / double(micros() - t_n));
-	Serial.print("\t");
-	wait();
-	Serial.println(1000000.0 / double(micros() - t_n));
+  Serial.print(1000000.0/double(micros()-t_n)); Serial.print("\t");
+  wait();
+  Serial.println(1000000.0/double(micros()-t_n));
 }
 
-void wait()
+void print_raw_sensor_data()
 {
-	while (micros() - t < loop_timer)
-	{
-	}
-	t = micros();
+  Serial.print(MgX); Serial.print('\t');
+  Serial.print(MgY); Serial.print('\t');
+  Serial.print(MgZ); Serial.print('\t');
+}
+
+void wait(){
+  while(micros()-t<loop_timer){}
+  t = micros();
 }
