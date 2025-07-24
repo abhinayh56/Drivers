e9945e369b3ac8596d8b47092270c03664348cf2
abhinayh56 <kumarabhinay53@gmail.com>
2025-07-24 19:21:02 +0530

    sfdgsdfg

---
+++ print_size.py
@@ -0,0 +1,8 @@
+Import("env")
+
+def print_size(source, target, env):
+    elf_path = env.subst("$BUILD_DIR/${PROGNAME}.elf")
+    print("\n=== Firmware Memory Usage ===")
+    env.Execute(f'arm-none-eabi-size "{elf_path}"')
+
+env.AddPostAction("$BUILD_DIR/${PROGNAME}.elf", print_size)
